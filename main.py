420d60cc09c8100ffd3e187915690c7b50e65814
shyamkumar <shyamkumar@qaid.co.in>
2025-10-23 11:01:22 +0530

    updated cors

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origin.strip() for origin in os.getenv("CORS-ORIGINS", "").split(",") if origin.strip()],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
